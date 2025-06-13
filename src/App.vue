93cd021e1efa27134b73761a784eaf4f1eb1f864
Dcoo <davidcoo@me.com>
2025-06-13 18:37:10 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="max-w-4xl mx-auto p-4">
     <h1 class="text-3xl font-bold text-center mb-8">
-      Art Work Tracker
+      Artwork Tracker
     </h1>
 
     <StatsDisplay :stats="currentStats" />
