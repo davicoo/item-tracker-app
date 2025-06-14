b6e14cd29534ba2e6d7054a00b1408c1ba00190d
Dcoo <davidcoo@me.com>
2025-06-14 16:12:43 -0400

    fix: restore intro GIF size

--- src/AppRoot.vue
+++ src/AppRoot.vue
@@ -4,6 +4,7 @@
     <div
       v-if="showLanding"
       class="fixed inset-0 flex items-center justify-center bg-black z-50"
+      style="position:fixed; inset:0; display:flex; align-items:center; justify-content:center; background-color:#000; z-index:50;"
     >
       <img
         src="/Uglygif.gif.GIF"
