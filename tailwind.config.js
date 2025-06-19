763bb2e58148e9636c3fc2663e65cd3c8e722827
Dcoo <davidcoo@me.com>
2025-06-18 23:58:31 -0400

    Add preline plugin for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import preline from 'preline/plugin'
+
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [preline],
 }
