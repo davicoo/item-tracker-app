cd38fdedd3a133796fda5a547080a718a383c81a
Dcoo <davidcoo@me.com>
2025-06-23 12:22:01 -0400

    Use HSDatepicker constructor

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -27,8 +27,12 @@ function updateValue(e: Event) {
 
 onMounted(() => {
   if (inputRef.value) {
-    // Initialize HSDatepicker
-    (window as any).HSDatepicker?.autoInit?.()
+    // Create HSDatepicker instance on mount
+    const HSDatepicker = (window as any).HSDatepicker
+    if (HSDatepicker) {
+      // eslint-disable-next-line new-cap
+      new HSDatepicker(inputRef.value)
+    }
     inputRef.value.addEventListener('change', updateValue)
     inputRef.value.addEventListener('input', updateValue)
     if (props.modelValue) {
