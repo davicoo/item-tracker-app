35e30e42c7196f763fd9e3ca4c57e561fc014e05
Dcoo <davidcoo@me.com>
2025-07-24 12:57:15 -0400

    Fix missing Scene for AnimatedBlob

--- src/components/AnimatedBlob.vue
+++ src/components/AnimatedBlob.vue
@@ -2,15 +2,17 @@
   <div class="fixed bottom-0 right-0 w-48 h-48 md:w-72 md:h-72 pointer-events-none z-10">
     <Renderer orbit-ctrl>
       <PerspectiveCamera :position="[0, 0, 5]" />
-      <AmbientLight :intensity="0.8" />
-      <PointLight :position="[10, 10, 10]" />
-      <Sphere :args="[1, 32, 32]">
-        <meshStandardMaterial
-          color="#7C3AED"
-          roughness="0.2"
-          metalness="0.6"
-        />
-      </Sphere>
+      <Scene>
+        <AmbientLight :intensity="0.8" />
+        <PointLight :position="[10, 10, 10]" />
+        <Sphere :args="[1, 32, 32]">
+          <meshStandardMaterial
+            color="#7C3AED"
+            roughness="0.2"
+            metalness="0.6"
+          />
+        </Sphere>
+      </Scene>
     </Renderer>
   </div>
 </template>
@@ -19,6 +21,7 @@
 import {
   Renderer,
   PerspectiveCamera,
+  Scene,
   AmbientLight,
   PointLight,
   Sphere
